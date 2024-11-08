5e94e2b87d95152e45e2853cc9461dc1e1ae438e
Joe Andrews <joeandrews92@gmail.com>
2024-11-08 12:43:12 -0800

    Edit loading message on homepage

--- src/routes/home.jsx
+++ src/routes/home.jsx
@@ -5,6 +5,7 @@ import { formatDate } from '../utils/formatDate'
 export default function Home() {
     const [posts, setPosts] = useState([]);
     const [loading, setLoading] = useState(true)
+    const [dots, setDots] = useState('');
 
     useEffect(() => {
         fetch("https://dazzling-elemental-airplane.glitch.me/posts", {                
@@ -23,10 +24,25 @@ export default function Home() {
             });
     }, []);
 
+    useEffect(() => {
+        const interval = setInterval(() => {
+          setDots((prev) => (prev.length < 3 ? prev + '.' : ''));
+        }, 500); // Change every 500ms
+    
+        return () => clearInterval(interval);
+      }, []);
+
 return (
      <div className="flex justify-center">
         <ul className="flex flex-col justify-center items-center gap-5 max-w-screen-xl">
-            {loading && <p>Loading...</p>}
+            {loading &&
+                <div className="text-center">    
+                <p className="text-gray-700">
+                    Fetching posts<span className="inline-block w-4 text-left">{dots}</span>
+                </p>
+                <p className="text-gray-600 text-sm mt-1">(This may take a minute)</p>
+                </div> 
+            }
             {posts.map((post) => (
                 <li key={post._id} className="shadow-md py-5 px-5 w-1/2">
                     <p className="text-xs text-right mb-1">{formatDate(post.timestamp)}</p>
